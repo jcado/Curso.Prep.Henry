6c86b1ff0e8820885248b81efaa5807ebce53726
john rafael carrillo dolugar <ganarconseo@gmail.com>
2021-12-23 16:49:55 -0500

    puntos resueltos de la actividad 4 hasta el punto 4

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,18 +3,25 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+  return array[0];
+
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+
+  return array[array.length - 1];
+
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+
+  return array.length;
 }
 
 
@@ -23,6 +30,13 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+ var nuevoArray=[];
+ for(var i = 0; i < array.length; i++){
+  nuevoArray[i] = array[i] + 1;
+}
+
+  return nuevoArray;
+  
 }
 
 
